8cecbf48fa27a4bd89927f9c88d86a4ec795b095
Igli milaqi <milaqiigli@gmail.com>
2019-05-07 18:19:11 +0200

    Controls for the gun implemented.

--- ProjectExposure/Assets/Scripts/Gun/Gun.cs
+++ ProjectExposure/Assets/Scripts/Gun/Gun.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Gun : MonoBehaviour
 {
 
+	public Action<Gun> OnChargeChanged;
+	
 	[SerializeField] 
 	private Vector2 m_gunColorRange = Vector2.zero;
 
@@ -18,7 +22,7 @@ public class Gun : MonoBehaviour
 	private KeyCode m_keyCode;
 
 	private Renderer m_renderer;
-	
+
 	private float m_hue = 0;
 	
 	// Use this for initialization
@@ -29,36 +33,44 @@ public class Gun : MonoBehaviour
 		m_renderer.material.color = m_gunColor;
 		
 		m_hue = GetColorHue(m_renderer.material.color) * 360;
+		if(OnChargeChanged != null) OnChargeChanged(this);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetKey(m_keyCode))
-		{
-
-			if (Input.GetKey(KeyCode.UpArrow) && m_hue < m_gunColorRange.y)
-			{
-				m_hue += m_gunChargeSpeed * Time.deltaTime;
-			}
-			else if (Input.GetKey(KeyCode.DownArrow) && m_hue > m_gunColorRange.x)
-			{
-				m_hue -= m_gunChargeSpeed * Time.deltaTime;
-			}
-
-			m_renderer.material.color = ChangeHue(m_gunColor, m_hue);
-		}
+		
 	}
 
 	
 	float GetColorHue(Color color)
 	{
-		float hue = 0;
+		float hue = 1;
 		float saturation = 0;
 		float value = 0;
 		
 		Color.RGBToHSV(color,out hue,out saturation,out value);
 		return hue;
+	}
+
+	public void IncreaseCharge()
+	{
+		if (m_hue < m_gunColorRange.y)
+		{
+			m_hue += m_gunChargeSpeed * Time.deltaTime;
+			m_renderer.material.color = ChangeHue(m_gunColor, m_hue);
+			if(OnChargeChanged != null) OnChargeChanged(this);
+		}
+	}
+
+	public void DecreaseCharge()
+	{
+		if (m_hue > m_gunColorRange.x)
+		{
+			m_hue -= m_gunChargeSpeed * Time.deltaTime;
+			m_renderer.material.color = ChangeHue(m_gunColor, m_hue);
+			if(OnChargeChanged != null) OnChargeChanged(this);
+		}
 	}
 
 	Color ChangeHue(Color color,float newHue)
@@ -73,5 +85,10 @@ public class Gun : MonoBehaviour
 		Debug.Log(newHue);
 		
 		return Color.HSVToRGB(newHue/360, saturation, value);
+	}
+
+	public float Hue()
+	{
+		return m_hue;
 	}
 }
---
+++ ProjectExposure/Assets/Scripts/UIScripts/PointerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerScript : MonoBehaviour
+{
+	[SerializeField] 
+	private Gun targetGun;
+	
+	[SerializeField]
+	private RectTransform m_barRectTransform;
+	
+	private RectTransform m_transform;
+
+	private float m_parentHalfWidth;
+	
+	// Use this for initialization
+	void Awake ()
+	{
+		m_transform = GetComponent<RectTransform>();
+		m_parentHalfWidth = m_barRectTransform.rect.width/2;
+
+		targetGun.OnChargeChanged += SetPositionBasedOnHue;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+
+	void SetPositionBasedOnHue(Gun gun)
+	{
+		float pos = ((gun.Hue()/300) * 2 -1) * m_parentHalfWidth ; //flip it also
+		
+		m_transform.anchoredPosition = new Vector2(pos,m_transform.anchoredPosition.y);
+	}
+}
