8645b2555f620a637d1f55b8aab299783e4eaeea
Ilyas <issatayev.ilyas@gmail.com>
2019-05-21 17:21:57 +0200

    final edits

---
+++ ProjectExposure/Assets/_Scripts/Movement/CurvePath/CurveWallker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurveWallker : MonoBehaviour {
+    public BezierCurve spline;
+
+    public float duration;
+
+    private float m_progress;
+
+    public bool lookForward;
+
+    private void Update()
+    {
+        m_progress += Time.deltaTime / duration;
+        if (m_progress > 1f)
+        {
+            m_progress = 1f;
+        }
+        Vector3 position = spline.GetPoint(m_progress);
+        transform.localPosition = position;
+        if (lookForward)
+        {
+            transform.LookAt(position + spline.GetDirection(m_progress));
+        }
+    }
+}
