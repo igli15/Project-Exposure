d6974a089c18aa600e27e83e0964299e7405f344
Ilyas <issatayev.ilyas@gmail.com>
2019-05-21 18:14:44 +0200

    Added tweening to the speed of moving through the curve

--- ProjectExposure/Assets/_Scripts/Movement/CurvePath/CurveWallker.cs
+++ ProjectExposure/Assets/_Scripts/Movement/CurvePath/CurveWallker.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
-public class CurveWallker : MonoBehaviour {
+public class CurveWallker : MonoBehaviour
+{
     public BezierCurve spline;
 
     public float duration;
 
     private float m_progress;
-
+    public float progress
+    {
+        set
+        {
+            m_progress = value;
+            if (m_progress > 1f)
+            {
+                m_progress = 1f;
+            }
+            Vector3 position = spline.GetPoint(m_progress);
+            transform.localPosition = position;
+            if (lookForward)
+            {
+                transform.LookAt(position + spline.GetDirection(m_progress));
+            }
+        }
+        get { return m_progress; }
+    }
     public bool lookForward;
-
+    public void Start()
+    {
+        DOTween.To(() => progress, x => progress = x, 1, duration).SetEase(Ease.InOutBounce).SetUpdate(true);
+    }
     private void Update()
     {
+        return;
         m_progress += Time.deltaTime / duration;
         if (m_progress > 1f)
         {
