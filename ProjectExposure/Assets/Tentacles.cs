d721372f38c73106eda394ef8f53682232a09c49
Guillaume <guil.king@t-online.de>
2019-05-23 00:33:58 +0200

    tentacles work

---
+++ ProjectExposure/Assets/Tentacles.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tentacles : Hittable {
+
+    [SerializeField]
+    private GameObject m_inkSpray;
+    public override void Hit(GunManager gunManager, float damage, Color gunColor)
+    {
+        Debug.Log("suck my tit");
+        m_inkSpray.gameObject.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
