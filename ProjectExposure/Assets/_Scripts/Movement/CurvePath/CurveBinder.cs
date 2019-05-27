53f2408ede117af93594c1e59e50e27d3c046b62
Ilyas <issatayev.ilyas@gmail.com>
2019-05-27 15:21:53 +0200

    added spline id

--- ProjectExposure/Assets/_Scripts/Movement/CurvePath/CurveBinder.cs
+++ ProjectExposure/Assets/_Scripts/Movement/CurvePath/CurveBinder.cs
@@ -6,8 +6,10 @@ public class CurveBinder : MonoBehaviour {
 
     [SerializeField]
     private BezierCurve m_spline;
+    public bool isActivated = false;
 
     private BezierCurve m_bindedSpline;
+    [SerializeField]
     public float progress;
 
     public void Reset()
@@ -15,14 +17,33 @@ public class CurveBinder : MonoBehaviour {
         m_bindedSpline = GetComponent<BezierCurve>();
     }
 
+    public void Update()
+    {
+        
+        if (!CurveWallker.instance) return;
+
+        if (Mathf.Abs(
+            CurveWallker.instance.progress - progress)<0.001f &&
+            isActivated&&
+            CurveWallker.instance.spline==m_spline)
+        {
+            Debug.Log("MyProgress: " + progress + " PlayerProgress: " + CurveWallker.instance.progress);
+            CurveWallker.instance.spline = GetComponent<BezierCurve>();
+            CurveWallker.instance.progress = 0;
+            isActivated = false;
+        }
+        
+    }
+
     public void StayAtSpline(float t)
     {
-        //if (m_spline != null) transform.position = m_spline.GetPoint(t);
 
         if (m_bindedSpline != null)
         {
-            m_bindedSpline.SetRawPoint(0,m_spline.GetPoint(t)-transform.position);
-            m_bindedSpline.SetRawPoint(1, m_spline.GetPoint(t) - transform.position + m_spline.GetDirection(t)*70 );
+            progress = t;
+            m_bindedSpline.SetRawPoint(0, m_spline.GetPoint(t) - transform.position);
+            m_bindedSpline.SetRawPoint(1, m_spline.GetPoint(t) - transform.position + m_spline.GetDirection(t) * 70);
         }
+        else { m_bindedSpline = GetComponent<BezierCurve>(); }
     }
 }
--- ProjectExposure/Assets/_Scripts/Movement/CurvePath/CurveWallker.cs
+++ ProjectExposure/Assets/_Scripts/Movement/CurvePath/CurveWallker.cs
@@ -5,7 +5,12 @@ using DG.Tweening;
 
 public class CurveWallker : MonoBehaviour
 {
+    [SerializeField]
     public BezierCurve spline;
+    [SerializeField]
+    public bool isPlayer=false;
+
+    public static CurveWallker instance;
 
     public float duration;
     public float speed = 2;
@@ -15,22 +20,13 @@ public class CurveWallker : MonoBehaviour
         set
         {
             m_progress = value;
-            if (m_progress > 1f)
-            {
-                m_progress = 1f;
-            }
-            Vector3 position = spline.GetPoint(m_progress);
-            transform.localPosition = position;
-            if (lookForward)
-            {
-                transform.LookAt(position + spline.GetDirection(m_progress));
-            }
         }
         get { return m_progress; }
     }
     public bool lookForward;
     public void Start()
     {
+        if (isPlayer) instance = this;
        // DOTween.To(() => progress, x => progress = x, 1, duration).SetEase(Ease.Linear).SetUpdate(true);
     }
     private void Update()
