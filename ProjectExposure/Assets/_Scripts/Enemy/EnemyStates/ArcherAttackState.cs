a8a3a5ba529541b38db2a8664befa92da76c06b8
Guillaume <guil.king@t-online.de>
2019-05-29 08:30:32 +0200

    no message

--- ProjectExposure/Assets/_Scripts/Enemy/EnemyStates/ArcherAttackState.cs
+++ ProjectExposure/Assets/_Scripts/Enemy/EnemyStates/ArcherAttackState.cs
@@ -29,7 +29,6 @@ public class ArcherAttackState : AbstractState<EnemyFSM>
         {
             if (m_isFreezed == false)
             {
-                Debug.Log("shoot!");
                 Shoot();
                 m_isFreezed = true;
             }
@@ -55,7 +54,6 @@ public class ArcherAttackState : AbstractState<EnemyFSM>
 
     void Shoot()
     {
-        Debug.Log("iam shootimg");
         transform.DOLookAt(Camera.main.transform.position, 0.5f);
         GetComponent<Animator>().SetTrigger("shoot");
         GameObject projectile = ObjectPooler.instance.SpawnFromPool("Projectile", transform.position, transform.rotation);
