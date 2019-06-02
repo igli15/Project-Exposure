9e6be0f1b64acd9da68f31338a55aed23a8ef635
Igli milaqi <milaqiigli@gmail.com>
2019-06-02 21:45:46 +0200

    created a new scene

---
+++ ProjectExposure/Assets/_Scripts/Gun/GunManagerStates/UltimateState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UltimateState : GunState 
+{
+	public override void Shoot()
+	{
+		if (target.isMouseDown) return; //if the mouse is clicking on the gun dont shoot!
+		
+		Hittable hittable = target.RaycastFromGuns();
+		
+		if(hittable != null)
+		{
+			
+			if (hittable.GetColor() == Color.white)
+			{
+				hittable.Hit(target,0,target.color);
+					
+			}
+			else
+			{
+				float m_damage = target.CalculateDamage(target.color, hittable.GetColor());
+				hittable.Hit(target,m_damage,target.color);
+			}
+		}
+	}
+}
