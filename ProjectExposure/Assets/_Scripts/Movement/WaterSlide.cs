f41c3aa88333443c46975b7793ea9c8c204b448b
Guillaume <guil.king@t-online.de>
2019-06-04 11:35:45 +0200

    partices

---
+++ ProjectExposure/Assets/_Scripts/Movement/WaterSlide.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSlide : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject m_waterStream;
+    [SerializeField]
+    private CurveBinder m_optionalPath;
+    [SerializeField]
+    private CurveBinder m_standardPath;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
