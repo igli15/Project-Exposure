95a47b4a14d431912d4e8c150e34a67deaf08dee
Igli milaqi <milaqiigli@gmail.com>
2019-06-04 12:19:03 +0200

    Added menu controller and wolrd space buttons.

---
+++ ProjectExposure/Assets/_Scripts/Menu Scripts/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			
+			if (Physics.Raycast(ray.origin,ray.direction, out hit, 2000))
+			{
+				WorldButton worldButton = hit.transform.GetComponent<WorldButton>();
+				if(worldButton != null) worldButton.Click();
+			}
+		}
+	}
+}
