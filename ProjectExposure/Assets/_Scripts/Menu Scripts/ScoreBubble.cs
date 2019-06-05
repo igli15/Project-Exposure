7a1a9665e1a84a85497a14735c50e517a9c93bcf
Igli milaqi <milaqiigli@gmail.com>
2019-06-05 09:47:32 +0200

    minor changes

--- ProjectExposure/Assets/_Scripts/Menu Scripts/ScoreBubble.cs
+++ ProjectExposure/Assets/_Scripts/Menu Scripts/ScoreBubble.cs
@@ -30,7 +30,7 @@ public class ScoreBubble : MonoBehaviour,IPointerDownHandler
 	{
 		if (m_rigidbody2D != null)
 		{
-			m_rigidbody2D.gravityScale = 0;
+			//m_rigidbody2D.gravityScale = 0;
 			m_rigidbody2D.velocity = Vector2.zero;
 			m_rectTransform.anchoredPosition = new Vector3(Random.Range(-20, 26), 0.5f, 0);
 		}
@@ -38,7 +38,7 @@ public class ScoreBubble : MonoBehaviour,IPointerDownHandler
 		DOVirtual.DelayedCall(Random.Range(1f,5.0f), delegate
 		{
 			m_bubbleText.text = s;
-			m_rigidbody2D.gravityScale = -0.2f;
+			m_rigidbody2D.velocity = Vector2.up * 2f;
 		});
 	}
 
@@ -46,14 +46,14 @@ public class ScoreBubble : MonoBehaviour,IPointerDownHandler
 	{
 		if (m_rigidbody2D != null)
 		{
-			m_rigidbody2D.gravityScale = 0;
+			//m_rigidbody2D.gravityScale = 0;
 			m_rigidbody2D.velocity = Vector2.zero;
 			m_rectTransform.anchoredPosition = new Vector3(Random.Range(-20, 26), 0.5f, 0);
 		}
 		
 		DOVirtual.DelayedCall(Random.Range(1,10f), delegate
 			{
-				m_rigidbody2D.gravityScale = -0.2f;
+				m_rigidbody2D.velocity = Vector2.up * 2f;
 			});
 	}
 
