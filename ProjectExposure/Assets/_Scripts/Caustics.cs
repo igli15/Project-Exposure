1fb0c3718b3a2d5f6fe66cae7687d574904e3e9b
Igli milaqi <milaqiigli@gmail.com>
2019-06-06 09:35:23 +0200

    stuff

---
+++ ProjectExposure/Assets/_Scripts/Caustics.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caustics : MonoBehaviour
+{
+	public float fps = 30.0f;
+	public Texture2D[] frames;
+
+	private int m_frameIndex;
+	private Projector m_projector;
+	
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_projector = GetComponent<Projector> ();
+		NextFrame();
+		InvokeRepeating ("NextFrame", 1 / fps, 1 / fps);
+	}
+	
+	void NextFrame(){
+		m_projector.material.SetTexture ("_ShadowTex", frames [m_frameIndex]);
+		m_frameIndex = (m_frameIndex + 1) % frames.Length;
+	}
+
+}
