97da16f7339f838ebaae46dd462838492802cb1e
Guillaume <guil.king@t-online.de>
2019-06-12 22:55:50 +0200

    YAS

--- ProjectExposure/Assets/_Scripts/Movement/WaterSlide.cs
+++ ProjectExposure/Assets/_Scripts/Movement/WaterSlide.cs
@@ -13,10 +13,31 @@ public class WaterSlide : MonoBehaviour {
     [SerializeField]
     private CurveBinder m_standardPath;
 
-    private void OnDestroy()
+    [SerializeField]
+    public Gem gem;
+
+    private bool m_stdActive = true;
+
+    public void Start()
     {
-        m_waterStream.SetActive(true);
-        m_optionalPath.isActivated = true;
-        m_standardPath.isActivated = true;
+        gem.onHit += ChangeActive;
+    }
+
+    public void ChangeActive()
+    {
+        if (m_stdActive == true)
+        {
+            m_waterStream.SetActive(true);
+            m_optionalPath.isActivated = true;
+            m_standardPath.isActivated = false;
+            m_stdActive = false;
+        }
+        else if (m_stdActive == false)
+        {
+            m_waterStream.SetActive(false);
+            m_optionalPath.isActivated = false;
+            m_standardPath.isActivated = true;
+            m_stdActive = true;
+        }
     }
 }
