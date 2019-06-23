e88d9da04011349e1df8b0bbf27967df74d0eccc
Igli milaqi <milaqiigli@gmail.com>
2019-06-23 17:01:10 +0200

    Combo meter is filled and reset properly

--- ProjectExposure/Assets/_Scripts/UIScripts/ComboMeter.cs
+++ ProjectExposure/Assets/_Scripts/UIScripts/ComboMeter.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class ComboMeter : MonoBehaviour
 {
@@ -25,19 +27,68 @@ public class ComboMeter : MonoBehaviour
 	{
 		m_image = GetComponent<Image>();
 		
-		IncreaseFill(20);
+		
+		
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			IncreaseFill(40);
+		}
+		
+		if (Input.GetKeyDown(KeyCode.B))
+		{
+			BreakCombo();
+		}
 	}
 
 
 	public void IncreaseFill(float degrees)
 	{
-		m_image.DOFillAmount(m_image.fillAmount + (degrees / 360.0f), 0.25f);
+		m_image.DOFillAmount(m_image.fillAmount + (degrees / 360.0f), 0.3f);
 		float fillDegrees = (m_image.fillAmount) * 360 + degrees;
 
 		int index = (int)(fillDegrees / 36.0f) - 1;
 		
-		if(index >= 0)
-		m_fillFrames[index].gameObject.SetActive(true);
+		if(index >= 0 && index <= 9) 
+			m_fillFrames[index].gameObject.SetActive(true);
+		
+		
+		if (index >= 9 && m_multiplier < 9)
+		{
+			IncreaseMultiplier();
+		}
+		
 
 	}
+
+	public void BreakCombo()
+	{
+		Reset();
+		m_multiplier = 1;
+	}
+	
+	private void IncreaseMultiplier()
+	{
+		Reset();
+		m_multiplier += 1;
+		m_multiplierText.text = m_multiplier + "X";
+		m_multiplierText.transform.DOPunchScale(Random.insideUnitCircle * 2, 0.2f);
+	}
+
+
+	private void Reset()
+	{
+		DOTween.KillAll();
+		for (int i = 0; i < m_fillFrames.Length; i++)
+		{
+			m_fillFrames[i].gameObject.SetActive(false);
+		}
+
+		m_image.fillAmount = 0;
+		m_multiplierText.text = "1X";
+	}
+	
 }
