7e5c463f55cdde6caebd42176c45fb8d519c3bfa
Ilyas <issatayev.ilyas@gmail.com>
2019-06-24 15:01:57 +0200

    func is done

--- ProjectExposure/Assets/_Scripts/BossViewScript.cs
+++ ProjectExposure/Assets/_Scripts/BossViewScript.cs
@@ -4,12 +4,37 @@ using UnityEngine;
 
 public class BossViewScript : MonoBehaviour {
 
+    public static BossViewScript instance;
+
+    [SerializeField]
+    List<TentacleBehaviour> m_tentacles;
 
     private void Start()
     {
+        instance = this;
         CurveWallker.instance.lookForward = false;
+        foreach (TentacleBehaviour tentacle in m_tentacles)
+        {
+            tentacle.onEnd += OnTentacleEnd;
+            tentacle.enabled = false;
+        }
+        ActivateNextTentacle();
     }
+
+    public void ActivateNextTentacle()
+    {
+        if (m_tentacles.Count == 0) return;
+        m_tentacles[0].enabled = true;
+        m_tentacles[0].ActivateTentacle();
+        m_tentacles.RemoveAt(0);
+    }
+
+    public void OnTentacleEnd(TentacleBehaviour tentacle)
+    {
+        ActivateNextTentacle();
+    }
+
     void Update () {
-        CurveWallker.instance.transform.LookAt(transform);	
+        CurveWallker.instance.transform.LookAt(transform);
 	}
 }
---
+++ ProjectExposure/Assets/_Scripts/TentacleBehaviour.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System;
+
+public class TentacleBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    [Range(0,1)]
+    public float range = 0.1f;
+    [SerializeField]
+    List<Crystal> crystals;
+    int m_crystalCount;
+    int m_currentCount;
+
+    public Action<TentacleBehaviour> onEnd;
+
+	void Start () {
+        foreach (Crystal c in crystals)
+        {
+            c.OnExplode += OnCrystalExplode;
+        }
+
+        m_crystalCount = crystals.Count;
+        m_currentCount = m_crystalCount;
+        
+    }
+	
+	void Update () {
+        Vector3 distance = ( BossViewScript.instance.transform.position- CurveWallker.instance.transform.position );
+        distance *= range;
+        Vector3 buffer = CurveWallker.instance.transform.position + distance;
+        buffer.y = transform.position.y;
+        transform.position = buffer;
+        transform.LookAt(CurveWallker.instance.transform);
+    }
+
+    void OnCrystalExplode(Crystal c)
+    {
+        m_currentCount--;
+        if (m_currentCount <= 0)
+        {
+            Debug.Log("OVER");
+            HideTentacle();
+        }
+    }
+
+    public void ActivateTentacle()
+    {
+        transform.position = new Vector3(transform.position.x, -5, transform.position.z);
+        transform.DOMoveY(6, 1);
+    }
+
+    public void HideTentacle()
+    {
+        enabled = false;
+        transform.DOLocalMoveY(-10, 1).OnComplete(() => { gameObject.SetActive(false); if (onEnd != null) onEnd(this); });
+    }
+}
