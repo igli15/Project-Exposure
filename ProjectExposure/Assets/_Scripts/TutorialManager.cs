d062f7119275f0bc926aadd50f62a8f13882fa7a
Igli milaqi <milaqiigli@gmail.com>
2019-06-25 12:49:04 +0200

    started working on tutroial manager.

---
+++ ProjectExposure/Assets/_Scripts/TutorialManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+	[SerializeField] private Crystal m_redCrystal;
+	[SerializeField] private Crystal m_greenCrystal;
+	[SerializeField] private Crystal m_blueCrystal;
+
+
+	[SerializeField] private Sprite[] m_hintSprites;
+	[SerializeField] private Sprite[] m_highlightSprites;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
