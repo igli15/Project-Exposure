5d932278aaa526b3c4d64a267f0347b9038901f5
Igli milaqi <milaqiigli@gmail.com>
2019-07-01 21:02:53 +0200

    STARTED IMPLEMENTING DISCRETE SLIDER.

---
+++ ProjectExposure/Assets/_Scripts/UIScripts/DiscreteSlider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscreteSlider : MonoBehaviour
+{
+	[SerializeField] private Sprite m_emptySprite;
+	[SerializeField] private Sprite m_filledSprite;
+
+	[SerializeField] private SliderElement[] m_sliderElements;
+	
+	private void Start()
+	{
+		
+	}
+}
---
+++ ProjectExposure/Assets/_Scripts/UIScripts/SliderElement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderElement : MonoBehaviour
+{
+	private bool m_isFull = false;
+
+	private DiscreteSlider m_parentSlider;
+
+	private SliderElement m_previousNeighbour;
+
+	private Image m_image;
+
+	private void Start()
+	{
+		m_image = GetComponent<Image>();
+	}
+
+	void Fill()
+	{
+		
+	}
+
+	void Empty()
+	{
+		
+	}
+	
+}
