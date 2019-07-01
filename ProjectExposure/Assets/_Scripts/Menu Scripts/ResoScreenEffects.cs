01b5c6d28506c0b3b964b543cda4bd2c7fe4d137
Igli milaqi <milaqiigli@gmail.com>
2019-07-01 17:27:23 +0200

    added some button effects

---
+++ ProjectExposure/Assets/_Scripts/Menu Scripts/ResoScreenEffects.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class ResoScreenEffects : MonoBehaviour
+{
+	
+	[SerializeField] private Transform m_buttonGroup;
+	[SerializeField] private Transform m_mainMenuPanel;
+	[SerializeField] private Transform m_leaderBoardPanel;
+
+	private Vector3 m_mainMenuInitScale;
+	private Vector3 m_leaderboardInitScale;
+
+	private void Start()
+	{
+		
+		Setup();
+	}
+
+	public void MoveButtonsDown()
+	{
+		Sequence s = DOTween.Sequence();
+		
+		s.Append(m_buttonGroup.DOMove(Vector3.zero, 1f).SetEase(Ease.Linear));
+
+		s.Append(m_mainMenuPanel.DOScale(m_mainMenuInitScale,0.5f));
+		s.Append(m_leaderBoardPanel.DOScale(m_leaderboardInitScale,0.5f));
+
+	}
+
+
+	private void Setup()
+	{
+		m_mainMenuInitScale = m_mainMenuPanel.transform.localScale;
+		m_leaderboardInitScale = m_leaderBoardPanel.transform.localScale;
+		m_mainMenuPanel.localScale = new Vector3(m_mainMenuPanel.localScale.x,0,m_mainMenuPanel.localScale.z);
+		m_leaderBoardPanel.localScale = new Vector3(m_leaderBoardPanel.localScale.x,0,m_leaderBoardPanel.localScale.z);
+	}
+}
