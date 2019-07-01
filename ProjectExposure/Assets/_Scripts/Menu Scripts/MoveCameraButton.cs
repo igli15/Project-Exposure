11c51a211b10254750998806d1f6419c2d36734c
Igli milaqi <milaqiigli@gmail.com>
2019-07-01 16:19:03 +0200

    camera moves to leaderboard dome.

---
+++ ProjectExposure/Assets/_Scripts/Menu Scripts/MoveCameraButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class MoveCameraButton : WorldButton
+{
+	[SerializeField] private Transform m_finalCameraPos;
+
+	public override void Click(Ray ray)
+	{
+		base.Click(ray);
+
+		Camera.main.transform.DOMove(m_finalCameraPos.position,4);
+		Camera.main.transform.DORotate(m_finalCameraPos.rotation.eulerAngles, 3);
+	}
+}
